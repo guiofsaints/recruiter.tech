bee49775adc64da490702f6e5b2542aae774cacc
Guilherme Santos <guilherme.dos.santos.91@gmail.com>
2021-02-17 22:51:31 -0300

    fix: fix scopes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,13 +6,13 @@ export default NextAuth({
   providers: [
     Providers.LinkedIn({
       clientId: process.env.LINKEDIN_CLIENT_ID,
-      clientSecret: process.env.LINKEDIN_CLIENT_SECRET
+      clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+      scope: ["r_emailaddress", "r_liteprofile"]
     }),
 
     Providers.Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      scope: ["r_emailaddress", "r_liteprofile"]
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
     // ...add more providers here
   ],
