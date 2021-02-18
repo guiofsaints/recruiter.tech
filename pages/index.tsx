319f9890dee919768ce1c989fb74fa102e3b5d36
Guilherme Santos <guilherme.dos.santos.91@gmail.com>
2021-02-17 22:00:12 -0300

    feat: add params

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { signIn, signOut, useSession } from "next-auth/client";
-import { Button } from "@material-ui/core";
+import { Box, Button, Container } from "@material-ui/core";
 import Footer from "../components/Section/Footer";
 import Benefits from "../components/Section/Benefits";
 import Header from "../components/Section/Header";
@@ -10,18 +10,25 @@ import Banner from "../components/Section/Banner";
 const Index = (): JSX.Element => {
   const [session, loading] = useSession();
 
+  console.log(session && session.user ? session.user : {});
+
   return (
     <>
       <Header />
       <Banner />
       <Benefits />
       <Footer />
-      {session && (
-        <>
-          Signed in as {session.user.email} <br />
-          <Button onClick={() => signOut()}>Sign out</Button>
-        </>
-      )}
+      <Container maxWidth="md">
+        <Box mt={2} alignContent="center" alignItems="center">
+          {session && (
+            <>
+              {session.user ? session.user.name : "Sem nome"} <br />
+              {session.user.email} <br />
+              <Button onClick={() => signOut()}>Sign out</Button>
+            </>
+          )}
+        </Box>
+      </Container>
     </>
   );
 };
